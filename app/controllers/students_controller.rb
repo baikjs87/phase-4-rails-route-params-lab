0e42ebe94c59f0f8e6ea23abf5dbeeeda62377d6
Jason Baik <baikjs87@gmail.com>
2022-08-17 20:39:00 -0400

    Working on bonus Q

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,15 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def search_by_name
+    name = if Student.find(first_name= params[:name]).or(Student.find(last_name= params[:name]))
+    end
+    render json: name
+  end
+
 end
